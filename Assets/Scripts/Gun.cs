3d4687c1caeda99250a9931804a1468ba525ac14
unknown <kmg0902@outlook.com>
2022-11-08 19:52:14 -0500

    Chapter 10 - Adding Damage Effects

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -9,16 +9,42 @@ public class Gun : MonoBehaviour
     public AudioClip liveFire;
     public AudioClip dryFire;
     protected float lastFireTime;
+    public float zoomFactor;
+    public int range;
+    public int damage;
+    private float zoomFOV;
+    private float zoomSpeed = 6;
     // Start is called before the first frame update
     void Start()
     {
-        lastFireTime = Time.time - 10;  
+        zoomFOV = Constants.CameraDefaultZoom / zoomFactor;
+        lastFireTime = Time.time - 10;
     }
 
     // Update is called once per frame
-    protected  virtual void Update()
+    protected virtual void Update()
     {
-        
+        if (Input.GetMouseButton(1))
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView,
+           zoomFOV, zoomSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Camera.main.fieldOfView = Constants.CameraDefaultZoom;
+        }
+    }
+
+    private void processHit(GameObject hitObject)
+    {
+        if (hitObject.GetComponent<Player>() != null)
+        {
+            hitObject.GetComponent<Player>().TakeDamage(damage);
+        }
+        if (hitObject.GetComponent<Robot>() != null)
+        {
+            hitObject.GetComponent<Robot>().TakeDamage(damage);
+        }
     }
 
     protected void Fire()
@@ -33,5 +59,11 @@ public class Gun : MonoBehaviour
             GetComponent<AudioSource>().PlayOneShot(dryFire);
         }
         GetComponentInChildren<Animator>().Play("Fire");
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            processHit(hit.collider.gameObject);
+        }
     }
 }
